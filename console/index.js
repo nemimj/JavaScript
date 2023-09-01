66a6a2f4400eecccc78979473c14ee4d06b104d9
nemi <nemimj123@gmail.com>
2023-09-01 11:40:17 +0530

    added console functions

--- console/index.js
+++ console/index.js
@@ -1,1 +1,18 @@
 console.log("Hello world");
+
+console.error("this is my error");
+
+console.warn("this is my warning");
+
+// Group
+console.group("Group A");
+console.log("hello");
+console.error("this is my group");
+console.groupEnd();
+
+// Table
+console.table({ name: "nemi", age: 22 });
+
+// Css styles in console
+const style = "padding: 10px; background-color:white; color:grey;";
+console.log("%cHello world", style);
