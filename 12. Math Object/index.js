9169dd442e0847f68180bad70a7c2f4888e9088d
nemi <nemimj123@gmail.com>
2023-09-03 10:21:36 +0530

    added math folder number folder date folder

---
+++ 12. Math Object/index.js
@@ -0,0 +1,13 @@
+let x;
+x = Math.sqrt(9);
+x = Math.abs(-5);
+x = Math.round(4.2); // output : 4 ( more than .5 ) 5
+x = Math.ceil(4.2); // output : 5
+x = Math.floor(4.9); // output : 4
+x = Math.pow(2, 3);
+x = Math.min(4, 5, 3);
+X = Math.max(4, 5, 3);
+
+// random number                //*     start from 1 to 99 if we not add the + 1
+x = Math.floor(Math.random() * 100 + 1);
+console.log(x);
---
+++ 13. Number Challenge/index.js
@@ -0,0 +1,8 @@
+x = Math.floor(Math.random() * 100 + 1);
+y = Math.floor(Math.random() * 50 + 1);
+
+console.log(`${x} + ${y} = ${x + y}`);
+console.log(`${x} - ${y} = ${x - y}`);
+console.log(`${x} * ${y} = ${x * y}`);
+console.log(`${x} / ${y} = ${x / y}`);
+console.log(`${x} % ${y} = ${x % y}`);
