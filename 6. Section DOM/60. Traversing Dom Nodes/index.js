1adc5b9b9d2e5cb5a18187855891e07f17e068fc
nemi <nemimj123@gmail.com>
2023-09-09 10:36:40 +0530

    added traversing node elements

---
+++ 6. Section DOM/60. Traversing Dom Nodes/index.js
@@ -0,0 +1,28 @@
+let output;
+const parent = document.querySelector(".parent");
+
+// child nodes are different from children element
+
+// chlidNodes consists of all the nodes that are inside parent , comments and whitespaces are also nodes
+output = parent.childNodes;
+output = parent.childNodes[0].textContent;
+output = parent.childNodes[0].nodeName;
+output = parent.childNodes[3].textContent;
+
+output = parent.firstChild;
+console.log(output);
+
+// Parent Node
+
+const child = document.querySelector(".child");
+
+output = child.parentNode;
+output = child.parentElement;
+
+// child
+
+const secondItem = document.querySelector(".child:nth-child(2)");
+output = secondItem.nextSibling;
+output = secondItem.previousSibling;
+
+console.log(output);
