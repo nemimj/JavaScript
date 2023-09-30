8728b821148ba9166d3fbf29cd166d4abdd4df21
nemi <nemimj123@gmail.com>
2023-09-30 12:26:59 +0530

    added current, watch and clear watch

--- 11. Web and Browser API's/101. Gelocation API/index.js
+++ 11. Web and Browser API's/101. Gelocation API/index.js
@@ -1,14 +1,51 @@
-const options = {};
+//* getCurrentPosition
+const options = {
+  enableHighAccuracy: true, // uses location if available
+  timeout: 5000, // Time to wait to stop trying for location
+  maximumAge: 0, // Don't use a cached position
+};
 
-navigator.geolocation.getCurrentPosition(resSuccess, resError, options);
+navigator.geolocation.getCurrentPosition(
+  CurrentPositionSuccess,
+  CurrentPositionError,
+  options
+);
 
-function resSuccess(pos) {
+function CurrentPositionSuccess(pos) {
   const {
     coords: { latitude, longitude },
   } = pos;
   console.log(latitude, longitude);
 }
 
-function resError(err) {
+function CurrentPositionError(err) {
+  console.log("is there any error" + err.code, err.message);
+}
+
+//* continously watching for the change in location
+
+const target = {
+  latitude: 72.4561,
+  longitude: -12.3652,
+};
+
+const watchId = navigator.geolocation.watchPosition(
+  WatchPositionSuccess,
+  WatchPositionError,
+  options
+);
+
+function WatchPositionSuccess(pos) {
+  const {
+    coords: { latitude, longitude },
+  } = pos;
+
+  if (target.latitude === latitude && target.longitude === longitude) {
+    console.log("Location is Matching");
+    navigator.geolocation.clearWatch(watchId);
+  }
+}
+
+function WatchPositionError(err) {
   console.log("is there any error" + err.code, err.message);
 }
